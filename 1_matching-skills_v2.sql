a6adcefe3235e62c3c56aea5fc8167da291d243d
Andres Felipe Forero Correa <aforeroc@unal.edu.co>
2022-10-15 17:00:05 -0500

    The first solution was refactored and its 'version 1'' was deleted

--- 1_matching-skills_v2.sql
+++
@@ -1,6 +0,0 @@
-SELECT candidate_id
-FROM
-  (SELECT * FROM candidates WHERE skill = 'Python' OR skill = 'Tableau' OR skill = 'PostgreSQL') Q1
-GROUP BY candidate_id
-HAVING COUNT(skill) = 3
-ORDER BY candidate_id;
