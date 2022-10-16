f9850b769303f7fd0b2cf4fa94280609a6471874
Andres Felipe Forero Correa <aforeroc@unal.edu.co>
2022-10-16 12:24:22 -0500

    4th problem was solved

---
+++ 4-laptop-mobile-viewership.sql
@@ -0,0 +1,4 @@
+SELECT 
+  COUNT(CASE WHEN device_type='laptop' THEN 1 END) AS laptop_views,
+  COUNT(CASE WHEN device_type='tablet' OR device_type='phone' THEN 1 END) AS mobile_views 
+FROM viewership;
