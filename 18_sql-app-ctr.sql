222e2ec67f621d65b3ac5811b3836155de8576f7
Andres Felipe Forero Correa <aforeroc@unal.edu.co>
2022-10-30 01:31:43 -0500

    18th solution

---
+++ 18_sql-app-ctr.sql
@@ -0,0 +1,8 @@
+SELECT
+  app_id,
+  ROUND(
+  SUM (CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) * 100.0 / 
+  SUM (CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2) AS count_impression
+FROM events
+WHERE EXTRACT(YEAR FROM timestamp) = 2022
+GROUP BY app_id;
